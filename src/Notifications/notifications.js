67cb6f471bb02e3bef87e6064aa0f384672bf8d2
Mesut Talebi <mesut.talebi@hotmail.com>
2017-07-08 16:35:43 +0300

    Added Src Folder

---
+++ src/Notifications/notifications.js
@@ -0,0 +1,116 @@
+﻿function ToastrDefaultOptions(position) {
+    var options = {
+        "closeButton": true,
+        "debug": false,
+        "positionClass": position === null ? "toast-bottom-right" : position,
+        "onclick": null,
+        "showDuration": "1000",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    return options;
+}
+
+/**
+ * Shows Taostr Growl
+ * @param {string} title Title of the toastr
+ * @param {string} msg Message to Toast can be html.
+ * @param {string} position position for toast ["toast-top-right","toast-top-full-width","toast-top-center", "toast-top-right","toast-bottom-right","toast-bottom-left","toast-bottom-full-width","toast-bottom-center" ]
+ * @param {string} type toast type ["success", "info", "warning", "error"]
+ */
+function ShowToast(title, msg, position, type) {
+    toastr[type](msg, title, ToastrDefaultOptions(position));
+}
+
+
+/**
+ * Shows a Gritter Growl Message
+ * @param {string} title Title for Growl
+ * @param {string} msg Message can be an HTML string
+ * @param {string} imageUrl Optional
+ * @param {object} callbacks a callbacks object, {beforeOpen : function(), afterOpen : function(e), beforeClose : function(e, manuelclose), afterClose : function() }
+ */
+function ShowGritter(title, msg, imageUrl, callbacks) {
+    if (callbacks === undefined)
+        callbacks = {};
+
+    if (imageUrl === undefined)
+        imageUrl = '';
+
+    if ($.gritter) {
+        $.gritter.add({            
+            title: title,            
+            text: msg,
+            // (string | optional) the image to display on the left
+            image: imageUrl,
+            sticky: false,
+            time: 8000,
+            // (function | optional) function called before it opens
+            before_open: callbacks.beforeOpen,
+            // (function | optional) function called after it opens
+            after_open: callbacks.afterOpen,
+            // (function | optional) function called before it closes
+            before_close: callbacks.beforeClose,
+            // (function | optional) function called after it closes
+            after_close: callbacks.afterClose
+        });
+    }
+}
+
+/**
+ * Shows Sweet Popup
+ * @param {string} title Title for Popup
+ * @param {string} msg Message for Popup
+ * @param {string} type popup type ["success", "info", "warning", "error"]
+ * @param {object} buttons buttons { okText: "OK", cancelText: "Cancel" }
+ * @param {object} callbacks a callbacks object, {confirm : function(), cancel : function()}
+ * @param {string} imageUrl Optional url for showing image in icon
+ */
+function ShowSweetPopup(title, msg, type, buttons, callbacks, imageUrl) {
+    var okButtonColor = "btn-success";
+
+    if (type === "error") { okButtonColor = "btn-danger"; }
+    else if (type === "warning") {okButtonColor = "btn-warning";}
+    else if (type === "info") { okButtonColor = "btn-info"; }
+    else {okButtonColor = "btn-primary"}
+
+    if (buttons === undefined)
+        buttons = {};
+
+    if (callbacks === undefined) {
+        swal({
+            title: title,
+            text: msg,
+            type: type,
+            confirmButtonText: buttons.okText !== undefined ? buttons.okText : "OK",
+            imageUrl: imageUrl,
+            allowOutsideClick : true
+        });
+    }
+    else {
+        swal({
+            title: title,
+            text: msg,
+            type: type,
+            showCancelButton: buttons.okText !== undefined,
+            confirmButtonClass: okButtonColor,
+            confirmButtonText: buttons.okText !== undefined ? buttons.okText : "Yes",
+            cancelButtonText: buttons.cancelText !== undefined ? buttons.cancelText : "No",
+            closeOnConfirm: false,
+            closeOnCancel: false
+        },
+            function (isConfirm) {
+                if (isConfirm) {
+                    callbacks.confirm();
+                } else {
+                    callbacks.cancel();
+                }
+            });
+    }
+}
